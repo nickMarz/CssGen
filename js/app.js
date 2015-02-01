f924cfb13a767a481114dadf64b28cbb6b3b4dd1
Nick Marazzo <nick@designmarz.com>
2015-02-01 14:06:29 -0800

    outputting live results to code block now working

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ setStart = function(){
 		$("#txtshd").css('text-shadow', '#ffffff 0px 0px 0px');
 		$("#txtshd").css('font-size', '22pt');
 		};
-
+// set Pixel to 
 sapp = function(){
 	var	 shadowX = $('#pixel1').val()+"px";
 	var	 shadowY = $('#pixel2').val()+"px";
@@ -15,36 +15,50 @@ sapp = function(){
 	var outCode = $("#txtshd");
 	$("#txtshd").css({'text-shadow': shadowColor+" "+shadowX+" "+shadowY+" "+ shadowBlur});
 	$("#txtshd").css('font-size', textSize);
+	
+	liveCodeOut = function(){
+	$('#codeOut').html( '<p>{<br>font-size: '+ textSize +'<br> text-shadow: '+ shadowX +' '+ shadowY +' '+ shadowBlur +' '+ shadowColor + '<br> }</p>');
+	};
 
 };
+
+// p { 
+//   text-shadow: 1px 1px 1px #000;
+// }
+
+
 
 $('body').change(shadowApply = function(){
 	$( "#pixel1" ).change(function() {
 		shadowX = $('#pixel1').val()+"px";
 		console.log( "Handler for .change() called. " + shadowX );
-		sapp();
+		
 	});
 	$( "#pixel2" ).change(function() {
 		shadowY = $('#pixel2').val()+"px";
 		console.log( "Handler for .change() called. " + shadowY );
-		sapp();
+		
 	});
 	$( "#pixel3" ).change(function() {
 		shadowBlur = $('#pixel3').val()+"px";	
 		console.log( "Handler for .change() called. " + shadowBlur );
-		sapp();
+		
 	});
 	$( "#pixel4" ).change(function() {
 		shadowColor = "#"+ $('#pixel4').val();	
 		console.log( "Handler for .change() called. " + shadowColor );
-		sapp();
+		
 	});
 	$( "#pixel5" ).change(function() {
 		textSize = $('#pixel5').val()+"pt";	
 		console.log( "Handler for .change() called. " + textSize );
-		sapp();
+		
 	});
+	sapp();
+	liveCodeOut();
 });
+
+
 
 
 setStart();
