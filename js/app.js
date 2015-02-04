cab1c0ec0f8a4af58e7637b8c8973402036a9692
Nick Marazzo <nick@designmarz.com>
2015-02-04 14:12:58 -0800

    HEX to rgba converter working

--- js/app.js
+++ js/app.js
@@ -1,29 +1,30 @@
 $(document).ready(function() {
-
-// Set defualt CSS values //
-setStart = function(){
-		$("#txtshd").css('text-shadow', '#ffffff 0px 0px 0px');
-		$("#txtshd").css('font-size', '22pt');
-		};
-// set Pixel to 
-sapp = function(){
 	var	 shadowX = $('#pixel1').val()+"px";
 	var	 shadowY = $('#pixel2').val()+"px";
 	var	 shadowBlur = $('#pixel3').val()+"px";
 	var	 shadowColor = "#"+ $('#pixel4').val();
 	var  textSize = $('#pixel5').val()+"pt";
 	var outCode = $("#txtshd");
+// Set defualt CSS values //
+setStart = function(){
+	$("#txtshd").css('text-shadow', '#ffffff 0px 0px 0px');
+	$("#txtshd").css('font-size', '22pt');
+};
+// Apply CSS styliong //
+sapp = function(){
 	$("#txtshd").css({'text-shadow': shadowColor+" "+shadowX+" "+shadowY+" "+ shadowBlur});
 	$("#txtshd").css('font-size', textSize);
-	
-	liveCodeOut = function(){
+};
+
+// Display Live Code //
+liveCodeOut = function(){
 	$('#codeOut').html( '<p>{<br>font-size: '+ 
 		textSize +';<br> text-shadow: '+ shadowX +' '+ shadowY +' '+ shadowBlur +
 		' '+ shadowColor + ';<br> }</p>');
-	};
 };
 
 
+// Listen for changes in the text fields //
 $('body').change(shadowApply = function(){
 	$( "#pixel1" ).change(function() {
 		shadowX = $('#pixel1').val()+"px";
--- js/longShadow.js
+++ js/longShadow.js
@@ -1,74 +1,98 @@
 $(document).ready(function() {
 
+leftRight = $('#longShd1').val();
+topDown = $('#longShd2').val();
+shadowLength = $('#longShd3').val();
+shadowColor = $('#longShd4').val();
+var shadowstring = [];
+lfRt = parseInt($('#longShd1').val());
+upDwn = parseInt($('#longShd2').val());
 
 // hex to RGB converter //
 
-
 hexToRgb = function(hex) {
-    // Expand shorthand form (e.g. '03F') to full form (e.g. '0033FF')
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-    	return r + r + g + g + b + b;
-    });
+	    // Expand shorthand form (e.g. '03F') to full form (e.g. '0033FF')
+	    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+	    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+	    	return r + r + g + g + b + b;
+	    });
 
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-    	r: parseInt(result[1], 16),
-    	g: parseInt(result[2], 16),
-    	b: parseInt(result[3], 16)
-    } : null;
-};
+	    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		var r = parseInt(result[1], 16);
+		var g = parseInt(result[2], 16);
+		var b = parseInt(result[3], 16);
+		var a = 1.0;
+
+		// console.log('test outout ------ 	rgba('+r+',',g+',',b+',',a+')');
+//  rgab( ##, ##, ##, ##)
+// 'rgba('+r+', '+g+', '+ b+', 'a+ ')';
+
+	    return  ('rgba('+r+','+g+','+b+','+a+ ')');
+	    	  // 'rgba('+r+','+g+','+b+','+a+')'
+
+		    // return result ? {
+		    // 	r: parseInt(result[1], 16),
+	    	// 	g: parseInt(result[2], 16),
+	    	// 	b: parseInt(result[3], 16),
+	    	// 	a: 1.0
+	    // }
+	    // : 
+	    // null;
+	};
+
+newColor = hexToRgb('#'+shadowColor);
+
 // hex to RGB converter //
 
 // Set defualt CSS values //
 setStart = function(){
 	$('#longShad').css('text-shadow', '#000000 10px 10px 10px');
 };
-// set longShd to 
-longShadow = function(){
-	var	 leftRight = $('#longShd1').val();
-	var	 topDown = $('#longShd2').val();
-	var	 gradValue = $('#longShd3').val();
-	var	 shadowColor = $('#longShd4').val();
-	var  GradCheck = shadowColor;
-	var	 gradYesNo = $('#insetBox').prop('checked');
-	var outCode = $('#longShad');
-	// $('#longShad').css({'text-shadow': '#'+shadowColor+' '+leftRight+'px'+' '+topDown+'px'});
-	
-	
-	liveCodeOut = function(){
-		$('#codeOut4').html( '<p>{<br>text-shadow: '+ shadowstring+ ';<br> }</p>');
-	};
-		hexToRgb(shadowColor);
+
+
+// set longShadow to 
+// longShadow = function(){
+// 	var	 leftRight = $('#longShd1').val();
+// 	var	 topDown = $('#longShd2').val();
+// 	var	 shadowLength = $('#longShd3').val();
+// 	var	 shadowColor = $('#longShd4').val();
+// 	var	 gradYesNo = $('#insetBox').prop('checked');
+// 	var outCode = $('#longShad');
+// };		
+
+liveCodeOut = function(){
+	$('#codeOut4').html( '<p>{<br>text-shadow: '+ shadowstring+ ';<br> }</p>');
+	console.log(newColor);
 };
 
 
-var shadowstring = [];
+
+
+
 
 longShadowGenerator = function(){
-	loopTimes = $('#longShd3').val();
+
+	var loopTimes = $('#longShd3').val();
 	var x = parseInt(loopTimes);
+	// newColor = hexToRgb('#'+shadowColor);
+	// var convertedColor = newColor;
 	shadowstring = []; //clear shadowstring on prior to new loop
 	for (var i = 0; i < x; i++) {
+			lfRt = parseInt($('#longShd1').val());
+			upDwn = parseInt($('#longShd2').val());
 		// console.log(loopTimes );
 		console.log(x);
 		// console.log("----------- Next Batch -----------");
 		if (i!==x-1) {
-			shadowstring.push( 'rgb(128,55,66) '+leftRight+i +'px '+topDown+i+'px, ');
+			shadowstring.push( newColor +' '+(lfRt+i) +'px '+(upDwn+i)+'px, ');
 		}
 		else{
-			shadowstring.push( 'rgb(128,55,66) '+i +'px '+i+'px');
+			shadowstring.push( newColor +' '+(lfRt+i) +'px '+(upDwn+i)+'px');
 		}
 	}//end for loop
-console.log(shadowstring.join(''));
-$('#longShad').attr('style','text-shadow:'+ shadowstring.join(''));
-	// console.log($('#longShad').css('text-shadow'));
-
+	console.log(shadowstring.join(''));
+	$('#longShad').attr('style','text-shadow:'+ shadowstring.join(''));
 };
-$('#longShad').css('text-shadow', shadowstring.join(''));
-console.log(shadowstring);
-// "style", "text-shadow: " + shadowstring
-
 
 
 $('body').change(shadowApply = function(){
@@ -83,8 +107,8 @@ $('body').change(shadowApply = function(){
 		
 	});
 	$( '#longShd3' ).change(function() {
-		gradValue = $('#longShd3').val();	
-		console.log( 'Handler for .change() called. ' + gradValue+'px' );
+		shadowLength = $('#longShd3').val();	
+		console.log( 'Handler for .change() called. ' + shadowLength+'px' );
 		
 	});
 	$( '#longShd4' ).change(function() {
@@ -98,29 +122,15 @@ $('body').change(shadowApply = function(){
 		console.log('hex to rgb '+	shadowColor);
 		
 	});
-	longShadow();
-	
+	// longShadow();
+	newColor = hexToRgb('#'+shadowColor);
 	longShadowGenerator();
 	liveCodeOut();
+
 });
 
 setStart();
 shadowApply();
-
-// liveCodeOut = function(){
-// 		$('#codeOut4').html( '<p>{<br>text-shadow: '+ leftRight+'px' +' '+ topDown+'px' +' '+
-// 			' '+ '#'+shadowColor + ';<br> }</p>' + shadowstring);
-// 	};
-
-
-
-	// $('#longShad').css({'text-shadow': shadowColor+' '+leftRight+'px'+' '+topDown+'px'+' '+ gradValue+'px' });
-	
-	
-	// liveCodeOut = function(){
-	// $('#codeOut4').html( '<p>{<br>text-shadow: '+ leftRight+'px' +' '+ topDown+'px' +' '+ gradValue+'px' +
-	// 	' '+ shadowColor + ';<br> }</p>');
-	// };
 
 
 });
